19da02302fda1e70e62c93b6a8c207e7d12e2199
Republic of Dalmatia <66219296+republicofdalmatia@users.noreply.github.com>
2021-05-24 02:56:08 +0200

    Fixed some typos and removed printers/print configuration logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,16 @@ useradd -m -s /usr/sbin/nologin -G sambashare $USERNAME
 echo "Creating Samba user $USERNAME and setting password $PASSWORD"
 printf "$PASSWORD\n$PASSWORD" | smbpasswd -sa $USERNAME && smbpasswd -e $USERNAME
 
-echo "Setting workgrou to $WORKGROUP"
+echo "Setting workgroup to $WORKGROUP"
 sed -i "s|@@@WORKGROUP@@@|$WORKGROUP|g" /etc/samba/smb.conf
 
 echo "Setting public share to $PUBLIC"
 sed -i "s|@@@PUBLIC@@@|$PUBLIC|g" /etc/samba/smb.conf
 
-echo "Setting printers to $PRINTERS"
+#echo "Setting printers to $PRINTERS"
 sed -i "s|@@@PRINTERS@@@|$PRINTERS|g" /etc/samba/smb.conf
 
-echo "Setting print drivers to $PRINT"
+#echo "Setting print drivers to $PRINT"
 sed -i "s|@@@PRINT@@@|$PRINT|g" /etc/samba/smb.conf
 
 echo "Running command: $@"
